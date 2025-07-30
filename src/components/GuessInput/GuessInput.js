8b634e4bf62f044af93b01453aaf1d450e6d9d42
Tibor Leupold <tibor@lpld.io>
2025-07-29 21:44:56 -0700

    Create basic input component, logs on submit

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function GuessInput() {
+  const [word, setWord] = React.useState("")
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(word)
+  }
+
+  return (
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+            id="guess-input"
+            type="text"
+            value={word}
+            onChange={event => {
+              setWord(event.target.value);
+            }}
+        />
+      </form>
+  )
+}
+
+export default GuessInput;
