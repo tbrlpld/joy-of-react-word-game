7d61ee955e315aebb5a303cc0c7ccc1538759f16
Tibor Leupold <tibor@lpld.io>
2025-07-30 16:34:20 -0700

    Render array of word in output

--- src/components/GuessesOutput/GuessesOutput.js
+++ src/components/GuessesOutput/GuessesOutput.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+class Guess {
+  constructor({id, word}) {
+    this.id = id
+    this.word = word
+  }
+}
+
 function GuessesOutput() {
-  return <div>Guesses output</div>;
+  const guesses = [
+    new Guess({id: crypto.randomUUID(), word: "hello"}),
+    new Guess({id: crypto.randomUUID(), word: "there"}),
+    new Guess({id: crypto.randomUUID(), word: "world"}),
+  ]
+
+  return (
+    <div className="guess-results">
+      {guesses.map(guess => <p key={guess.id} className="guess">{guess.word}</p>)}
+    </div>
+  )
 }
 
 export default GuessesOutput;
