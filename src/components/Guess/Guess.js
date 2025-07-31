30bc3e37dc9bee86667348f10da80271b85ccba9
Tibor Leupold <tibor@lpld.io>
2025-07-30 18:10:01 -0700

    Add comment about index use

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -8,6 +8,7 @@ function Guess({word}) {
   const cells = range(5).map(index => {
     const letter = letters[index] || ""
     return (
+      // We are ok to use the index here, because the letters won't be reordered.
       <span key={index} className="cell">{letter}</span>
     )
   })
