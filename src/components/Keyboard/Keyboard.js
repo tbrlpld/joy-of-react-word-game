052776587f404d18d1109cb89cc45e91b02a3d9f
Tibor Leupold <tibor@lpld.io>
2025-08-04 14:29:27 -0700

    Use a public static method in stead of a private update method

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { checkGuess } from '../../game-helpers'
+import React from 'react'
 
 const keys = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
-];
+]
 
 class KeyStates {
-  static UNUSED = "unused"
-  static MISPLACED = "misplaced"
-  static CORRECT = "correct"
-  static INCORRECT = "incorrect"
+  static UNUSED = 'unused'
+  static MISPLACED = 'misplaced'
+  static CORRECT = 'correct'
+  static INCORRECT = 'incorrect'
 
   static ALLOWED_TRANSITONS = {
-    "unused": [KeyStates.INCORRECT, KeyStates.MISPLACED, KeyStates.CORRECT],
+    'unused': [KeyStates.INCORRECT, KeyStates.MISPLACED, KeyStates.CORRECT],
     // When there are multiple occurrences, there can be a correct one and a incorrect one.
     // If the correct ones position is after the incorrect one, we might find the incorrect one first.
     // Thus, we need to allow to upgrade from first one we found (incorrect) to second one (correct).
-    "incorrect": [KeyStates.CORRECT],
-    "misplaced": [KeyStates.CORRECT],
-    "correct": [],
+    'incorrect': [KeyStates.CORRECT],
+    'misplaced': [KeyStates.CORRECT],
+    'correct': [],
   }
 
-  constructor ({checkedGuesses}) {
+  constructor ({ checkedGuesses }) {
     this.#setInitialStates()
-    this.#updateStates({checkedGuesses: checkedGuesses})
+    this.#updateStates({ checkedGuesses: checkedGuesses })
   }
 
-  #setInitialStates() {
+  #setInitialStates () {
     keys.forEach(row => {
       row.forEach(key => {
         this[key] = KeyStates.UNUSED
@@ -36,28 +35,36 @@ class KeyStates {
     })
   }
 
-  #updateStates({checkedGuesses}) {
+  #updateStates ({ checkedGuesses }) {
     checkedGuesses.forEach(checkedGuess => {
       checkedGuess.forEach(checkedLetter => {
-        this.#updateStatus({
-          key: checkedLetter.letter,
-          newStatus: checkedLetter.status,
-        })
+        const key = checkedLetter.letter
+        this[key] = KeyStates.getNextStatus({ current: this[key], candidate: checkedLetter.status })
       })
     })
   }
 
-  #updateStatus({key, newStatus}) {
-    const currentStatus = this[key]
-    const allowedTransitionsForCurrentStatus = KeyStates.ALLOWED_TRANSITONS[currentStatus]
-    if (allowedTransitionsForCurrentStatus.includes(newStatus)) {
-      this[key] = newStatus
+  /**
+   * Get the next status from the current and a candidate.
+   *
+   * Returns the candidate if a valid choice. Otherwise, returns the current status if no change should occur.
+   *
+   * @param current
+   * @param candidate
+   * @returns {*}
+   */
+  static getNextStatus ({ current, candidate }) {
+    const allowedTransitionsForCurrentStatus = KeyStates.ALLOWED_TRANSITONS[current]
+    if (allowedTransitionsForCurrentStatus.includes(candidate)) {
+      return candidate
+    } else {
+      return current
     }
   }
 }
 
-function Keyboard({checkedGuesses}) {
-  const keyStates = new KeyStates({checkedGuesses})
+function Keyboard ({ checkedGuesses }) {
+  const keyStates = new KeyStates({ checkedGuesses })
 
   return (
     <div className="keyboard">
@@ -74,7 +81,7 @@ function Keyboard({checkedGuesses}) {
         })
       }
     </div>
-  );
+  )
 }
 
-export default Keyboard;
+export default Keyboard
