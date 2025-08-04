221e146519923101055d3d88934367b0bf9a6e8c
Tibor Leupold <tibor@lpld.io>
2025-08-04 15:01:13 -0700

    Fix format

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,46 +1,44 @@
-import React from 'react';
+import React from 'react'
 
-import { WORDS } from '../../data';
+import { WORDS } from '../../data'
 import { checkGuess, getGameStatus } from '../../game-helpers'
-import { sample } from '../../utils';
-import GuessInput from "../GuessInput";
-import GuessesOutput from "../GuessesOutput";
-import Guess from "../Guess";
-import { GuessedWord } from './Game.models';
+import { sample } from '../../utils'
+import GuessInput from '../GuessInput'
+import GuessesOutput from '../GuessesOutput'
+import Guess from '../Guess'
+import { GuessedWord } from './Game.models'
 import WonBanner from '../WonBanner'
 import LostBanner from '../LostBanner'
 import Keyboard from '../Keyboard'
-import guess from '../Guess'
-
 
 // Pick a random word on every pageload.
-const answer = sample(WORDS);
+const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+console.info({ answer })
 
-function Game() {
+function Game () {
   const [guessedWords, setGuessedWords] = React.useState([])
 
-  function guessWord(word) {
-    const nextGuessedWord = new GuessedWord({word: word})
+  function guessWord (word) {
+    const nextGuessedWord = new GuessedWord({ word: word })
     setGuessedWords([...guessedWords, nextGuessedWord])
   }
 
   const guessElements = []
-  const checkedGuesses =  []
+  const checkedGuesses = []
   const words = []
 
   guessedWords.forEach(guessedWord => {
-    const checkedGuess = checkGuess({guess: guessedWord.value, answer: answer})
+    const checkedGuess = checkGuess({ guess: guessedWord.value, answer: answer })
 
     guessElements.push(
-      <Guess key={guessedWord.id} checkedGuess={checkedGuess} />
+      <Guess key={guessedWord.id} checkedGuess={checkedGuess}/>
     )
     checkedGuesses.push(checkedGuess)
     words.push(guessedWord.value)
   })
 
-  const gameStatus = getGameStatus({words: words, answer: answer})
+  const gameStatus = getGameStatus({ words: words, answer: answer })
 
   return (
     <>
@@ -48,14 +46,14 @@ function Game() {
         {guessElements}
       </GuessesOutput>
 
-      <GuessInput submitNewGuess={guessWord} disabled={gameStatus !== "playing"}/>
+      <GuessInput submitNewGuess={guessWord} disabled={gameStatus !== 'playing'}/>
 
-      { gameStatus === "won" && <WonBanner numOfGuesses={guessedWords.length} />}
-      { gameStatus === "lost" && <LostBanner answer={answer} /> }
+      {gameStatus === 'won' && <WonBanner numOfGuesses={guessedWords.length}/>}
+      {gameStatus === 'lost' && <LostBanner answer={answer}/>}
 
       <Keyboard checkedGuesses={checkedGuesses}/>
     </>
   )
 }
 
-export default Game;
+export default Game
