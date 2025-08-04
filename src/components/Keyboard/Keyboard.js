349dabf8fc72aa37c0fe9a43b5b322295a9c8f50
Tibor Leupold <tibor@lpld.io>
2025-08-04 08:44:06 -0700

    Add initial key states

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -6,15 +6,37 @@ const keys = [
   ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 ];
 
+class KeyState {
+  constructor({letter}){
+    this.letter = letter
+    this.state = "unused"
+  }
+}
+
+function getInitialKeyStates() {
+  const states = {}
+  keys.forEach(row => {
+    row.forEach(key => {
+      states[key] = new KeyState({letter: key})
+    })
+  })
+  return states
+}
+
 function Keyboard() {
+  const [keyStates, setKeyStates] = React.useState(getInitialKeyStates)
+
+  console.log(keyStates)
+
   return (
     <div className="keyboard">
       {
-        keys.map(row => {
+        keys.map((row, index) => {
           return (
-            <div className="keyboard__row">
+            // The rows don't change, so it's ok to used index here.
+            <div key={index} className="keyboard__row">
               {row.map(key => {
-                return <div className="keyboard__key">{key}</div>
+                return <div key={key} className="keyboard__key">{key}</div>
               })}
             </div>
           )
